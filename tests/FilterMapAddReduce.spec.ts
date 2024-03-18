f7bfbf016b85631a8856e1e988886d59826771b6
Ines Garrote <alu0101512297@ull.edu.es>
2024-03-18 11:03:14 +0000

    tests hechos

---
+++ tests/FilterMapAddReduce.spec.ts
@@ -0,0 +1,28 @@
+import "mocha";
+//import { expect } from "chai";
+import pkg from 'chai';
+const { expect } = pkg;
+import { FilterMapAddReduce } from "../src/FilterMapAddReduce.js";
+
+describe("FilterMapAddReduce", () => {
+  it("filter", () => {
+    const filterMapAddReduce = new FilterMapAddReduce();
+    const pred = (x: number) => x % 2 === 0; // Filter even numbers
+    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result = filterMapAddReduce.filter(pred, list);
+    expect(result).to.eql([2, 4, 6, 8, 10]);
+  });
+  it("map", () => {
+    const filterMapAddReduce = new FilterMapAddReduce();
+    const mapFunc = (x: number) => x + 1; // Map: Add 1 to each element
+    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result = filterMapAddReduce.map(mapFunc, list);
+    expect(result).to.eql([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+  });
+  it("reduce", () => {
+    const filterMapAddReduce = new FilterMapAddReduce();
+    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result = filterMapAddReduce.reduce(list);
+    expect(result).to.equal(55);
+  });
+});
