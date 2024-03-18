fec43ba127eac42197fd2270bf0192a2d9ebd49a
Inés Garrote <91554163+inesgarrote@users.noreply.github.com>
2024-03-18 10:20:48 +0000

    haciendo ejercicio

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/T5K9tzcv)
 
 # Práctica 9
-Inés Garrote Fontenla alu0101512297@ull.edu.es  
+
+Inés Garrote Fontenla alu0101512297@ull.edu.es
 
 [![Coverage Status](https://coveralls.io/repos/github/ULL-ESIT-INF-DSI-2324/ull-esit-inf-dsi-23-24-prct09-filesystem-magic-app-inesgarrote/badge.svg?branch=main)](https://coveralls.io/github/ULL-ESIT-INF-DSI-2324/ull-esit-inf-dsi-23-24-prct09-filesystem-magic-app-inesgarrote?branch=main)
 
---
+++ src/ejercicio_clase.ts
@@ -0,0 +1,184 @@
+/*
+Implemente, a través del patrón Template Method, un algoritmo que permita llevar a cabo los siguientes pasos:
+Operación de filtrado sobre una lista de números (fíjese en cómo funciona la función filter de TypeScript e implemente su operación de filtrado sin hacer uso de dicha función filter). La operación de filtrado recibe como argumento un predicado lógico que se evalúa sobre cada elemento de la lista, esto es, una función que de resultar ser verdadera para el elemento correspondiente por el que se vaya iterando, haga que dicho elemento pase a formar parte de la lista devuelta por la operación de filtrado. En caso de que el predicado sea falso, el elemento se descartará de la lista resultante.
+Operación map sobre la lista de números resultante de la operación de filtrado (sin hacer uso del propio método map proporcionado por TypeScript). En este caso, de un modo similar a la operación filter, la operación map recibe también una función como argumento que permite transformar cada elemento de la lista de entrada aplicándole dicha función.
+Operación reduce (sin hacer uso del propio método reduce proporcionado por TypeScript) sobre la lista resultante de haber aplicado el map. En este caso, la lista de entrada se reduce a un único número.
+En la clase base abstracta, la operación reduce debe ser abstracta (así tendremos que implementarla obligatoriamente en las subclases), mientras que las operaciones filter y map deberán proporcionar un comportamiento por defecto que podría sobreescribirse, si se deseara, en las subclases. Luego, en las subclases, que podrían ser, por ejemplo, FilterMapAddReduce, FilterMapSubReduce, FilterMapProdReduce y FilterMapDivReduce, particularice la implementación de la operación reduce según corresponda. También añada algunos métodos de enganche (hooks) entre los pasos que haya definido en su método de plantilla.
+*/
+
+
+/**
+ * @brief Clase abstracta que define el comportamiento de un algoritmo que realiza operaciones de filtrado, mapeo y reducción sobre una lista de elementos.
+ * @tparam T Tipo de los elementos de la lista de entrada.
+ * @tparam U Tipo de los elementos de la lista de salida.
+ */
+abstract class AbstractFilterMapReduce<T, U> {
+ filter(pred: (item: T) => boolean, list: T[]): T[] {
+    const result: T[] = [];
+    for (const item of list) {
+      if (pred(item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  }
+  map(func: (item: T) => U, list: T[]): U[] {
+    const result: U[] = [];
+    for (const item of list) {
+      result.push(func(item));
+    }
+    return result;
+  }
+  abstract reduce(list: U[]): U;
+
+  templateMethod(
+    pred: (item: T) => boolean,
+    mapFunc: (item: T) => U,
+    list: T[],
+  ): U {
+    const filteredList = this.filter(pred, list);
+    const mappedList = this.map(mapFunc, filteredList);
+    return this.reduce(mappedList);
+  }
+}
+
+/**
+ * @brief Clase concreta que implementa el algoritmo de filtrado, mapeo y reducción sobre una lista de números. En el método de reducción, se realiza la operación de suma.
+ * @tparam number Tipo de los elementos de la lista de entrada y de la lista de salida.
+ * @tparam number Tipo de los elementos de la lista de entrada y de la lista de salida.
+ */
+class FilterMapAddReduce extends AbstractFilterMapReduce<number, number> {
+  filter(pred: (item: number) => boolean, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      if (pred(item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  }
+
+  map(func: (item: number) => number, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      result.push(func(item));
+    }
+    return result;
+  }
+
+  reduce(list: number[]): number {
+    let sum = 0;
+    for (const item of list) {
+      sum += item;
+    }
+    return sum;
+  }
+}
+
+
+/**
+ * @brief Clase concreta que implementa el algoritmo de filtrado, mapeo y reducción sobre una lista de números. En el método de reducción, se realiza la operación de resta.
+ * @tparam number Tipo de los elementos de la lista de entrada y de la lista de salida.
+ */
+class FilterMapSubReduce extends AbstractFilterMapReduce<number, number> {
+  filter(pred: (item: number) => boolean, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      if (pred(item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  }
+
+  map(func: (item: number) => number, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      result.push(func(item));
+    }
+    return result;
+  }
+
+  reduce(list: number[]): number {
+    let sub = 0;
+    for (const item of list) {
+      sub -= item;
+    }
+    return sub;
+  }
+}
+
+/**
+ * @brief Clase concreta que implementa el algoritmo de filtrado, mapeo y reducción sobre una lista de números. En el método de reducción, se realiza la operación de multiplicación.
+ * @tparam number Tipo de los elementos de la lista de entrada y de la lista de salida.
+ */
+class FilterMapProdReduce extends AbstractFilterMapReduce<number, number> {
+  filter(pred: (item: number) => boolean, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      if (pred(item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  }
+
+  map(func: (item: number) => number, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      result.push(func(item));
+    }
+    return result;
+  }
+
+  reduce(list: number[]): number {
+    let prod = 1;
+    for (const item of list) {
+      prod *= item;
+    }
+    return prod;
+  }
+}
+
+/**
+ * @brief Clase concreta que implementa el algoritmo de filtrado, mapeo y reducción sobre una lista de números. En el método de reducción, se realiza la operación de división.
+ * @tparam number Tipo de los elementos de la lista de entrada y de la lista de salida.
+ */
+class FilterMapDivReduce extends AbstractFilterMapReduce<number, number> {
+  filter(pred: (item: number) => boolean, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      if (pred(item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  }
+
+  map(func: (item: number) => number, list: number[]): number[] {
+    const result: number[] = [];
+    for (const item of list) {
+      result.push(func(item));
+    }
+    return result;
+  }
+
+  reduce(list: number[]): number {
+    let div = 1;
+    for (const item of list) {
+      div /= item;
+    }
+    return div;
+  }
+}
+
+
+
+// Example usage:
+const filterMapAddReduce = new FilterMapAddReduce();
+const pred = (x: number) => x % 2 === 0; // Filter even numbers
+const mapFunc = (x: number) => x + 1; // Map: Add 1 to each element
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result = filterMapAddReduce.templateMethod(pred, mapFunc, list);
+console.log(result); // Output: 35 (Sum of filtered and mapped elements)
